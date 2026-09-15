b9b44dda1bd7aa25b5bd1d10f7191692afbe34c3
Tony-koder <tonyn.koder@gmail.com>
2026-09-15 13:52:23 +0200

    Changed the code to add some styling, a search bar, and other minor things

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GameLogger
 
-## GameLogger is supposed to be a try at making a functional video game backlog and history overview
+## GameLogger is supposed to be a try at making a functional video game backlog and history overview.
+
+## It's pretty barebones, but at the moment you can enter names of games, write the status of them (Beaten, still in backlog, etc), a picture of them, as well as your rating of them out of 10. You can also search for games you've entered!
--- script.js
+++ script.js
@@ -1,7 +1,52 @@
 const gameLoggerForm = document.querySelector("#gameLoggerForm")
 const gameContainer = document.querySelector("#gameContainer")
+const searchBar = document.querySelector("#searchBar")
 
 // Code for making the GameLogger work
+
+searchBar.addEventListener("input", (e) => {
+    createGameCards(e.target.value)
+})
+
+function createGameCards( where) {
+    let searchFunction;
+    if (where) {
+        const getGames = JSON.parse(localStorage.getItem("gameinfo"))
+        
+        searchFunction = getGames.filter(search => search.gameName.includes(where))
+        
+    } else {
+        searchFunction = JSON.parse(localStorage.getItem("gameinfo"))
+    }
+    
+    
+    
+    gameContainer.innerHTML = ""
+    
+    searchFunction.forEach(({gameName, status, rating, picture}) => {
+        const containDiv = document.createElement("div")
+        
+        const gameNameP = document.createElement("p")
+        gameNameP.textContent = "Name: " + gameName
+        
+        const statusP = document.createElement("p")
+        statusP.textContent = "Status: " + status
+        
+        const ratingP = document.createElement("p")
+        ratingP.textContent = "Rating: "+ rating + "/10"
+
+        const img = document.createElement("img")
+        img.src = picture
+        
+        containDiv.append(gameNameP, statusP, ratingP, img)
+        gameContainer.append(containDiv)
+        
+        
+    })
+    
+}
+
+createGameCards()
 gameLoggerForm.addEventListener("submit", (e) => {
     e.preventDefault();
     
@@ -18,7 +63,6 @@ gameLoggerForm.addEventListener("submit", (e) => {
             gameArray.push(game)
         })
 
-        gameArray.push(data)
     }
     gameArray.push(data)
     
@@ -26,33 +70,3 @@ gameLoggerForm.addEventListener("submit", (e) => {
 
     createGameCards()
 })
-
-
-
-function createGameCards() {
-    const getGames = JSON.parse(localStorage.getItem("gameinfo")) || []
-
-    gameContainer.innerHTML = ""
-
-    getGames.forEach(({gameName, status, rating, picture}) => {
-        const containDiv = document.createElement("div")
-
-        const gameNameP = document.createElement("p")
-        gameNameP.textContent = gameName
-        
-        const statusP = document.createElement("p")
-        statusP.textContent = status
-
-        const ratingP = document.createElement("p")
-        ratingP.textContent = rating
-
-        const img = document.createElement("img")
-        img.src = picture
-
-        containDiv.append(gameNameP, statusP, ratingP, img)
-        gameContainer.append(containDiv)
-
-
-    })
-
-}
